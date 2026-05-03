e39d90fa1bbd286b07533fbe45e46bf13b69d108
FelipeBarbur <felipe.barburramirez@unicolombo.edu.co>
2026-05-03 13:49:07 -0500

    chore: configure steamy app base and capacitor dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'steamy-app',
+  appId: 'com.felipebarbur.steamyapp',
+  appName: 'My Steamy App',
   webDir: 'www'
 };
 
